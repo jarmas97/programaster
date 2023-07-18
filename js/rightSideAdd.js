3b63e3ae15bb8d74cc37ae814686593431f83895
Jarosław Masłowski <jerry.maslowski97@gmail.com>
2023-07-18 23:59:07 +0200

    version 18.07.2023

---
+++ js/rightSideAdd.js
@@ -0,0 +1,8 @@
+// atOptions = {
+//     'key' : 'b66828e0389921a96e319b8bff3e559a',
+//     'format' : 'iframe',
+//     'height' : 300,
+//     'width' : 160,
+//     'params' : {}
+//   };
+//   document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://resistpajamas.com/b66828e0389921a96e319b8bff3e559a/invoke.js"></scr' + 'ipt>');
